691a05d5260e63136bbcdd58060c6bcc5ad6e0ce
Mishieck <mishieckmwale@gmail.com>
2021-04-21 11:12:39 +0200

    refactor: use element.className instead of element.classList in Component.apply.classes
    - Append new classes to element.className instead of adding each class individually via element.classList
    - This method is more performant than the previous one as it adds all classes at once

--- src/apply/classes.js
+++ src/apply/classes.js
@@ -1,5 +1,5 @@
 export const applyClasses = async function (classEntries) {
   await this.apply.custom(classEntries, (element, classes) => {
-    for (const className of classList) element.classList.add(className);
+    element.className += ` ${classes.join(" ")}`;
   });
 };

808eb1b2fea8668a6672d9393e52a889d7f42c18
Mishieck <mishieckmwale@gmail.com>
2021-04-23 13:02:30 +0200

    feat: add limitAwait to assetManager module exports

--- src/asset-manager/asset-manager.js
+++ src/asset-manager/asset-manager.js
@@ -1,9 +1,6 @@
 import { importModule } from "./import-module.js";
 import { fetchAsset } from "./fetch-asset.js";
 import { prefetch } from "./prefetch.js";
+import { limitAwait } from "./limit-await.js";
 
-export const assetManager = {
-  import: importModule,
-  fetch: fetchAsset,
-  prefetch
-};
+export const assetManager = { importModule, fetchAsset, prefetch, limitAwait };

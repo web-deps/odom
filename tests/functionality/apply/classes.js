83ed30d4527383da938dffc8d06a4af77850160f
Mishieck <mishieckmwale@gmail.com>
2021-05-08 16:43:58 +0200

    tests(classes): add Component.apply.classes test to functionality test file
    - Add Component.apply.classes test to functionality.html
    - Put classes in a map in test module

--- tests/functionality/apply/classes.js
+++ tests/functionality/apply/classes.js
@@ -3,14 +3,19 @@ import { createComponent } from "/src/main.js";
 
 const classes = async () => {
   const markup = /* html */`<div class="class-0"></div>`;
-  const _classes = ["class-1", "class-2"];
+  const scopeClasses = ["class-1", "class-2"];
+
+  const _classes = {
+    ":scope": scopeClasses
+  };
+
   const options = { markup, classes: _classes };
   const Classes = await createComponent(options);
   const scope = Classes.scope;
   let passed = true;
   if (!scope.classList.contains("class-0")) passed = false;
 
-  for (const className of _classes) {
+  for (const className of scopeClasses) {
     if (!scope.classList.contains(className)) passed = false;
   };
 

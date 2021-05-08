90f974a1f3215d1e02434a68028440a8f80a85dc
Mishieck <mishieckmwale@gmail.com>
2021-05-08 17:02:19 +0200

    tests(apply): add test for Component.apply.eventListeners to functionality test file
    - Add test for Component.apply.eventListeners to functionality.html
    - Rewrite listener in _eventListeners in test module
    - Trigger click event using JavaScript in test module

--- tests/functionality/apply/event-listeners.js
+++ tests/functionality/apply/event-listeners.js
@@ -3,16 +3,20 @@ import { createComponent } from "/src/main.js";
 
 const eventListeners = async () => {
   const markup = /* html */`<button>Click</button>`;
+  let message = "Passed";
 
   const _eventListeners = {
     "button": [{
       type: "click",
-      listener: () => alert("Passed")
+      listener: () => {
+        console.info(message);
+      }
     }] 
   };
 
   const options = { markup, eventListeners: _eventListeners };
   const EventListeners = await createComponent(options);
+  EventListeners.scope.click();
   return EventListeners;
 };
 

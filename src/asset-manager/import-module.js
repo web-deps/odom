ce8ae0238a5b7276a673360a281d838454e3cb84
Mishieck <mishieckmwale@gmail.com>
2021-05-10 09:49:43 +0200

    fix(assetManager.importModule): fix destructuring bug
    - Set second parameter to an empty object is it is not provided

--- src/asset-manager/import-module.js
+++ src/asset-manager/import-module.js
@@ -1,4 +1,4 @@
-export const importModule = async (src, { name, construct, props }) => {
+export const importModule = async (src, { name, construct, props } = {}) => {
 	const module = await import(src);
 	const asset = name ? module[name] : Object.values(module)[0];
 	return (typeof asset === "function" && construct) ? asset(props) : asset;

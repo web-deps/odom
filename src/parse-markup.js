4ca4baac487f64ad66931a1c359fdf7305ab2163
Mishieck <mishieckmwale@gmail.com>
2021-05-13 14:13:36 +0200

    fix(parse-markup): do not add state to scope
    - Do not add state to scope in Component.parseMarkup

--- src/parse-markup.js
+++ src/parse-markup.js
@@ -4,6 +4,5 @@ import { parseMarkup as parse } from "./dom/parse-markup.js";
 export const parseMarkup = async function (markup, middleware) {
   this.scope = await parse({ markup, middleware });
   this.scope.setAttribute("acom-scope", this.id);
-  if (this.state) this.scope.acom.state = this.state;
   return this.scope;
 };

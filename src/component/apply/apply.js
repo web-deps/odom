14697b73591166fda7330d54f0b4a2707883bdc4
Mishieck <mishieckmwale@gmail.com>
2021-05-16 14:07:27 +0200

    refactor: rename Component.apply method param 'param' to 'map'

--- src/component/apply/apply.js
+++ src/component/apply/apply.js
@@ -12,8 +12,8 @@ export const apply = function () {
   return {
     styles: async (_styles, middleware) => styles.call(this, _styles, middleware),
     inlineStyles: async (map) => this.apply.custom(map, applyInlineStyles),
-    eventListeners: async param => eventListeners.call(this, param),
-    run: async (param) => await run.call(this, param),
+    eventListeners: async map => eventListeners.call(this, map),
+    run: async (options) => await run.call(this, options),
     custom: async (data, action, options) => await custom.call(this, data, action, options),
     classes: async (map) => await this.apply.custom(map, applyClasses),
     attributes: async (map) => await this.apply.custom(map, applyAttributes),

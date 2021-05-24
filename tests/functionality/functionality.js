e611eed8f72f10dc9c5968f0ef564a42754f551d
Mishieck <mishieckmwale@gmail.com>
2021-05-24 16:42:12 +0200

    tests(functionality): comment out tests

--- tests/functionality/functionality.js
+++ tests/functionality/functionality.js
@@ -6,49 +6,48 @@ import utils from "./utils/utils.js";
 import attributes from "./attributes/attributes.js";
 import middleware from "./middleware/middleware.js";
 
-
 // Tests for the API
 // createComponent
 // api.createComponent();
 
 // Component
-  // apply
-  // api.component.apply.attributes();
-  // api.component.apply.classes();
-  // api.component.apply.custom();
-  // api.component.apply.eventListeners();
-  // api.component.apply.inlineStyles();
-  // api.component.apply.mutations.major();
-  // api.component.apply.mutations.minor();
-  // api.component.apply.run();
-  // api.component.apply.styles();
+// apply
+// api.component.apply.attributes();
+// api.component.apply.classes();
+// api.component.apply.custom();
+// api.component.apply.eventListeners();
+// api.component.apply.inlineStyles();
+// api.component.apply.mutations.major();
+// api.component.apply.mutations.minor();
+// api.component.apply.run();
+// api.component.apply.styles();
 
-  // parseMarkup
-  // api.component.parseMarkup();
+// parseMarkup
+// api.component.parseMarkup();
 
-  // render
-  // api.component.render();
+// render
+// api.component.render();
 
-  // select
-  // api.component.select();
+// select
+// api.component.select();
 
-  // setProps
-  // api.component.setProps();
+// setProps
+// api.component.setProps();
 
-  // transform
-  // api.component.transform.display();
-  // api.component.transform.insertComponents();
-  // api.component.transform.insertData();
-  // api.component.transform.insertMarkup();
-  // api.component.transform.insertNodes();
-  // api.component.transform.insertSlots();
-  // api.component.transform.insertText();
-  // api.component.transform.loading();
-  // api.component.transform.map();
-  // api.component.transform.multiple();
-  // api.component.transform.presence();
-  // api.component.transform.run();
-  // api.component.transform.visibility();
+// transform
+// api.component.transform.display();
+// api.component.transform.insertComponents();
+// api.component.transform.insertData();
+// api.component.transform.insertMarkup();
+// api.component.transform.insertNodes();
+// api.component.transform.insertSlots();
+// api.component.transform.insertText();
+// api.component.transform.loading();
+// api.component.transform.map();
+// api.component.transform.multiple();
+// api.component.transform.presence();
+// api.component.transform.run();
+// api.component.transform.visibility();
 
 // importComponent
 // api.importComponent();
@@ -67,7 +66,7 @@ import middleware from "./middleware/middleware.js";
 
 // Tests for assets
 // assets.components.esm();
-assets.components.html();
+// assets.components.html();
 // assets.markup();
 // assets.node();
 // assets.text();
@@ -97,11 +96,11 @@ assets.components.html();
 
 // Tests for middleware
 // markup
-  // middleware.markup.converter();
-  // middleware.markup.custom();
-  // middleware.markup.parser();
+// middleware.markup.converter();
+// middleware.markup.custom();
+// middleware.markup.parser();
 
 // styles
-  // middleware.styles.custom();
-  // middleware.styles.postprocessor();
-  // middleware.styles.preprocessor();
+// middleware.styles.custom();
+// middleware.styles.postprocessor();
+// middleware.styles.preprocessor();

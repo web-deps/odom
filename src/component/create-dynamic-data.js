6472e5e6086b36492db52ab35dcd0763b3fe6a1d
Mishieck <mishieckmwale@gmail.com>
2021-05-28 17:38:28 +0200

    feat: set value from attribute directly on dynamic data
    - Set value from attribute update to avoid recursive update

--- src/component/create-dynamic-data.js
+++ src/component/create-dynamic-data.js
@@ -1,55 +1,65 @@
 export const createDynamicData = async (data) => {
-	const update = value => value;
-	
-	for (const name in data) {
-		const value = data[name];
-		const includeUpdator = typeof value !== "object" || !("data" in value && "updates" in value);
-		
-		if (includeUpdator) {
-			data[name] = {
-				data: value,
-				updates: [update]
-			};
-		};
+  const update = (value) => value;
+  const setValue = createValueSetter(data);
 
-		data[name].elements = [];
-	};
-	
-	return new Proxy(data, {
-		get(target, name) {
-			if (!Reflect.has(target, name)) {
-				if (name === "addElement") {
-					const addElement = (dataName, elementData) => {
-						const datum = Reflect.get(target, dataName);
-						datum.elements.push(elementData);
-					};
+  for (const name in data) {
+    const value = data[name];
+    const includeUpdator = typeof value !== "object" || !("data" in value && "updates" in value);
 
-					return addElement;
-				};
+    if (includeUpdator) {
+      data[name] = {
+        data: value,
+        updates: [update]
+      };
+    }
 
-				// console.error(`Could not find dynamic data with named "${name}".`);
-				return undefined;
-			};
-			
-			return target[name].data;
-		},
-		set(target, name, value) {
-			if (!Reflect.has(target, name)) {
-				return console.error(`Could not find data named "${name}".`);
-			};
+    data[name].elements = [];
+  }
 
-      if (target[name].data === value) return true;
-			for (const update of target[name].updates) value = update(value);
-			target[name].data = value;
+  return new Proxy(data, {
+    get(target, name) {
+      if (!Reflect.has(target, name)) {
+        if (name === "addElement") {
+          const addElement = (dataName, elementData) => {
+            const datum = Reflect.get(target, dataName);
+            datum.elements.push(elementData);
+          };
+
+          return addElement;
+        } else if (name === "setValueFromAttribute") {
+          return setValue;
+        }
+
+        // console.error(`Could not find dynamic data with named "${name}".`);
+        return undefined;
+      }
+
+      return target[name].data;
+    },
+    set(target, name, value) {
+      if (!Reflect.has(target, name)) {
+        return console.error(`Could not find data named "${name}".`);
+      }
+
+      if (!setValue(name, value)) return true;
 
       if (data[name].elements.length) {
-        for (const { target, attributeName } of data[name].elements) {
-					if (target.getAttribute(attributeName) === value) continue;
+        for (const {target, attributeName} of data[name].elements) {
+          if (target.getAttribute(attributeName) === value) continue;
           if (target) target.setAttribute(attributeName, value);
-        };
-      };
+        }
+      }
 
-			return true;
-		}
-	});
+      return true;
+    }
+  });
+};
+
+const createValueSetter = (target) => {
+  return (name, value) => {
+    if (target[name].data === value) return false;
+    for (const update of target[name].updates) value = update(value);
+    target[name].data = value;
+    return true;
+  };
 };
--- src/component/transform/insert/data.js
+++ src/component/transform/insert/data.js
@@ -1,9 +1,8 @@
-import { observeMutations } from "../../../dom/observe-mutations.js";
-import { getData } from "../../../get-data.js";
+import {observeMutations} from "../../../dom/observe-mutations.js";
+import {getData} from "../../../get-data.js";
 
-
-export const insertData = async ({ element, props, data, methods, dynamicData, skip }) => {
-  const { elements, attributes } = skip;
+export const insertData = async ({element, props, data, methods, dynamicData, skip}) => {
+  const {elements, attributes} = skip;
 
   if (elements) {
     let skipElement = false;
@@ -12,30 +11,30 @@ export const insertData = async ({ element, props, data, methods, dynamicData, s
       if (element.hasAttribute(attribute)) {
         skipElement = true;
         break;
-      };
-    };
+      }
+    }
 
     if (skipElement) return;
-  };
+  }
 
-  const insert = async ({ name, value }) => {
+  const insert = async ({name, value}) => {
     if (attributes && attributes.indexOf(name) !== -1) return;
-    if (/^(:{1,2}@data)/.test(value)) handleDynamicData({ element, attribute: [name, value], data: dynamicData });
-    const datum = await getData({ selector: value, data, methods, props, dynamicData });
+    if (/^(:{1,2}@data)/.test(value)) handleDynamicData({element, attribute: [name, value], data: dynamicData});
+    const datum = await getData({selector: value, data, methods, props, dynamicData});
     if (datum !== value) element.setAttribute(name, datum);
   };
 
   for (const attribute of element.attributes) await insert(attribute);
 };
 
-const handleDynamicData = ({ element, attribute: [name, value], data } ) => {
+const handleDynamicData = ({element, attribute: [name, value], data}) => {
   const doubleBind = value.startsWith("::");
   const selector = value.replace(/:{1,2}@data\./, "");
   const datum = data[selector];
 
   const update = (newValue) => {
     if (newValue === data[selector]) return;
-    data[selector] = newValue;
+    data.setValueFromAttribute(selector, newValue);
   };
 
   const elementData = {
@@ -49,11 +48,11 @@ const handleDynamicData = ({ element, attribute: [name, value], data } ) => {
 
   observeMutations(
     element,
-    mutations => {
-      const { target, attributeName } = mutations[0];
+    (mutations) => {
+      const {target, attributeName} = mutations[0];
       update(target.getAttribute(attributeName));
     },
-    { attributes: true, attributeFilter: [name] }
+    {attributes: true, attributeFilter: [name]}
   );
 
   if (name !== "value") return;
@@ -65,13 +64,13 @@ const handleDynamicData = ({ element, attribute: [name, value], data } ) => {
     if (tagName === element.tagName) {
       isInputControl = true;
       break;
-    };
-  };
+    }
+  }
 
   if (!isInputControl) return;
   const EVENT_TYPES = ["input", "keyup", "change"];
-  
+
   for (const eventType of EVENT_TYPES) {
     element.addEventListener(eventType, (event) => update(event.target.value));
-  };
+  }
 };

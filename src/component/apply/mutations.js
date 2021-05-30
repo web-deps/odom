d0c723c5313f85193518d950ce356a277bbe97e6
Mishieck <mishieckmwale@gmail.com>
2021-05-30 11:00:26 +0200

    feat: allow removal of subtree in the DOM during mutations

--- src/component/apply/mutations.js
+++ src/component/apply/mutations.js
@@ -1,28 +1,32 @@
 export const applyMutations = async (element, { type, mutator, preserve }) => {
-	if (type === "major") await major(element, mutator, preserve);
-	else if (type === "minor") minor(element, mutator);
-	else throw new Error(`Invalid mutation type "${type}"`);
+  if (type === "major") await major(element, mutator, preserve);
+  else if (type === "minor") minor(element, mutator);
+  else throw new Error(`Invalid mutation type "${type}"`);
 };
 
-const major = async (element, mutator, { subtree = true, observers = true } = {}) => {
-	let subject = null, placeholder = null;
-	
-	if (observers) {
-		const clone = element.cloneNode(true);
-		element.replaceWith(clone);
-		if (!subtree) element.innerHTML = "";
-		subject = element;
-		placeholder = clone;
-	} else {
-		subject = element.cloneNode(subtree);
-		placeholder = element;
-	};
-	
-	const mutant = await mutator(subject);
-	placeholder.replaceWith(mutant);
+const major = async (element, mutator, { subtree = true, observers = true, documentSubtree = true } = {}) => {
+  let subject = null,
+    placeholder = null;
+
+  if (observers) {
+    const clone = element.cloneNode(documentSubtree);
+    element.replaceWith(clone);
+    if (!subtree) element.innerHTML = "";
+    subject = element;
+    placeholder = clone;
+  } else {
+    subject = element.cloneNode(subtree);
+    if (!documentSubtree) {
+      placeholder = subject.cloneNode(false);
+      element.replaceWith(placeholder);
+    } else placeholder = element;
+  }
+
+  const mutant = await mutator(subject);
+  placeholder.replaceWith(mutant);
 };
 
 const minor = (element, mutator) => {
-	const mutate = () => mutator(element);
-	requestAnimationFrame(mutate);
+  const mutate = () => mutator(element);
+  requestAnimationFrame(mutate);
 };

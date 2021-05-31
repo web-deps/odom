b59df2cad786066509d361ffeb278e3459b76f34
Mishieck <mishieckmwale@gmail.com>
2021-05-31 15:43:20 +0200

    tests: use interpolation in HTML components

--- tests/functionality/api/import-component.js
+++ tests/functionality/api/import-component.js
@@ -2,15 +2,22 @@ import { importComponent } from "/src/main.js";
 import * as Acom from "/src/main.js";
 import logResult from "/tests/functionality/log-result.js";
 
-
 const _importComponent = async () => {
   window.Acom = Acom; // Simulate CDN
-  const component = await importComponent("/tests/functionality/assets/assets/html-component.html");
-  const ImportComponent = await component();
-  const passed = !!ImportComponent.scope;
+  const src = "/tests/functionality/assets/assets/html-component.html";
+  const res = await fetch(src);
+  const componentText = await res.json();
+  let passed = await isComponent(await importComponent({ src }));
+  const result = await isComponent(await importComponent({ file: componentText }));
+  passed = passed && result;
+
   logResult(passed);
   return ImportComponent;
 };
 
+const isComponent = async (component) => {
+  const ImportComponent = await component();
+  return !!ImportComponent.scope;
+};
 
 export default _importComponent;
--- tests/functionality/assets/components/html.js
+++ tests/functionality/assets/components/html.js
@@ -1,10 +1,9 @@
 import { createComponent, importComponent } from "/src/main.js";
 
-
 const html = async () => {
   window.Acom = { createComponent }; // Simulate CDN
   const src = "/tests/functionality/assets/assets/html-component.html";
-  const component = await importComponent(src);
+  const component = await importComponent({ src });
   const DemoComponent = await component();
   let passed = !!DemoComponent.scope;
 
@@ -13,6 +12,5 @@ const html = async () => {
 
   return DemoComponent;
 };
-
 
 export default html;
